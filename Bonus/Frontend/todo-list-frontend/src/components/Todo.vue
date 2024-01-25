5bc455a296de6b6ae4d6c4e1c6ae9a37587d2c1f
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-01-25 19:05:29 +0100

    mostrare lo stato del task: se completato, barrare il testo

---
+++ Bonus/Frontend/todo-list-frontend/src/components/Todo.vue
@@ -0,0 +1,37 @@
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Todo',
+  data() {
+    return {
+      todoArr: [],
+    };
+  },
+  mounted() {
+    axios.get('http://localhost/php-todo-list-json/Backend/').then((res) => {
+      this.todoArr = res.data;
+    }
+    )
+  }
+}
+</script>
+
+<template>
+  <h1>Ecco la golosa lista dei compiti da svolgere: </h1>
+  <ul>
+    <li v-for="(compito, index) in todoArr" :key="index">
+      <span :class="compito.done ? 'text' : ''">{{ compito.compito }}</span>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+.text {
+  text-decoration: line-through;
+}
+
+.read-the-docs {
+  color: #888;
+}
+</style>
